be775e14758f5f325910d20aa592ca7622e7a96b
Syed Hassan Ullah <syedhassanullah.0900@gmail.com>
2025-01-12 13:32:03 +0500

    changes

--- src/Components/Buttons/MainButton.js
+++ src/Components/Buttons/MainButton.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+
 
 function MainButton() {
   return (
-    <div>MainButton</div>
+    <motion.div
+    whileHover={{ scale: 1.2 }}
+    whileTap={{ scale: 0.95 }}
+    style={{ cursor: 'pointer', padding: '15px', backgroundColor: 'white', color: 'red', width: '250px', textAlign: 'center', marginTop: '30px' }}
+    className='mainButton'
+>
+    GET STARTED
+</motion.div>
   )
 }
 
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap'
 import homeImage from '../Embroidery-Landing-Page-1-Parrot-600x600-TinyPNG-Compressed2.png'
 import { motion } from 'framer-motion';
 import { FadeIn } from '../Varient/Varient'
+import MainButton from './Buttons/MainButton';
 
 
 function Hero() {
@@ -24,13 +25,7 @@ function Hero() {
                             <h3>Byond Digitizing <br />Agency</h3>
                             <h1>We are working on </h1>
                             <span className='chngecontent'></span>
-                            <motion.div
-                                whileHover={{ scale: 1.5 }}
-                                whileTap={{ scale: 0.95 }}
-                                style={{ cursor: 'pointer', padding: '15px', backgroundColor: 'white', color: 'red', width: '250px', textAlign: 'center', marginTop: '30px' }}
-                            >
-                                GET STARTED
-                            </motion.div>
+                            <MainButton/>
                         </motion.div>
                         <div className='about-image-sec col-md-4 heroimg' md={6} sm={12}>
                             <img src={homeImage} alt='HERO' className='about-image img-fluid' ></img>
