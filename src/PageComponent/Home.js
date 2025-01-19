009d01b0ad01f1309c12db1b006792b755617c3d
Syed Hassan Ullah <syedhassanullah.0900@gmail.com>
2025-01-19 22:27:26 +0500

    changes

--- src/PageComponent/Home.js
+++ src/PageComponent/Home.js
@@ -3,7 +3,7 @@ import MainLayout from '../Layout/MainLayout';
 import Hero from '../Components/Hero';
 import ContactSec from '../Components/ContactSec';
 import AboutSec from '../Components/AboutSec';
-import Sec1 from '../Components/Sec1';
+// import Sec1 from '../Components/Sec1';
 import {Service} from '../Components/Services/Service'
 import FlagSlider from '../Components/Slider/FlagSlider';
 import LogoSlider from '../Components/Slider/LogoSlider'
